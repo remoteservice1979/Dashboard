67debb54f973b8f80d5e6277d2f82308b542de30
INBLR02-LINUX-13 <remoteservuce1979@gmail.com>
2019-12-15 16:34:26 +0530

    location change

--- public/precache-manifest.9f1c0b4886e0bb623c90a9004cdf0b3c.js
+++ public/precache-manifest.9f1c0b4886e0bb623c90a9004cdf0b3c.js
@@ -1,51 +1,51 @@
 self.__precacheManifest = [
   {
     "revision": "c2a5f414c16e37e0c5b6",
-    "url": "/static/css/main.8f7516e2.chunk.css"
+    "url": "static/css/main.8f7516e2.chunk.css"
   },
   {
     "revision": "c2a5f414c16e37e0c5b6",
-    "url": "/static/js/main.c2a5f414.chunk.js"
+    "url": "static/js/main.c2a5f414.chunk.js"
   },
   {
     "revision": "adbe2d723fafa01cb960",
-    "url": "/static/css/1.99e695e7.chunk.css"
+    "url": "static/css/1.99e695e7.chunk.css"
   },
   {
     "revision": "adbe2d723fafa01cb960",
-    "url": "/static/js/1.adbe2d72.chunk.js"
+    "url": "static/js/1.adbe2d72.chunk.js"
   },
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "eb6be414b75c412c13eeee2d2e8390cb",
-    "url": "/static/media/react-logo.eb6be414.png"
+    "url": "static/media/react-logo.eb6be414.png"
   },
   {
     "revision": "bd6820f1a4015e9645e470ea8e7b1501",
-    "url": "/static/media/anime3.bd6820f1.png"
+    "url": "static/media/anime3.bd6820f1.png"
   },
   {
     "revision": "9fcf69e5359987c9b1545aae1c2c5faa",
-    "url": "/static/media/emilyz.9fcf69e5.jpg"
+    "url": "static/media/emilyz.9fcf69e5.jpg"
   },
   {
     "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
-    "url": "/static/media/nucleo.f0b489a5.woff"
+    "url": "static/media/nucleo.f0b489a5.woff"
   },
   {
     "revision": "5987dd12fea78ce5f97ae601b08ec03c",
-    "url": "/static/media/nucleo.5987dd12.woff2"
+    "url": "static/media/nucleo.5987dd12.woff2"
   },
   {
     "revision": "03ef1918e505c3e3471f9369ef7a638f",
-    "url": "/static/media/nucleo.03ef1918.eot"
+    "url": "static/media/nucleo.03ef1918.eot"
   },
   {
     "revision": "b17a118e13e53558658b681a0ebdad82",
-    "url": "/static/media/nucleo.b17a118e.ttf"
+    "url": "static/media/nucleo.b17a118e.ttf"
   },
   {
     "revision": "59c5686ee46ff84351fc2dcbfdcef1af",
